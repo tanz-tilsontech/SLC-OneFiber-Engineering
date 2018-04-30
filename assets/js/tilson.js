82cf4cc3322e001acdda90035ce407537e791c95
tanz-tilsontech <36584403+tanz-tilsontech@users.noreply.github.com>
2018-04-30 15:49:36 -0600

    asdf

--- assets/js/tilson.js
+++ assets/js/tilson.js
@@ -206,7 +206,8 @@ var highlightLayer = L.geoJson(null, {
 });
 
 
-var featureLayer = L.esri.featureLayer('https://tilsonwebdraco.3-gislive.com/arcgis/rest/services/SLClld/Tilsonslc_lld/MapServer/109', {
+var featureLayer = L.esri.featureLayer({
+  url: 'https://tilsonwebdraco.3-gislive.com/arcgis/rest/services/SLClld/Tilsonslc_lld/MapServer/109/query?where=centroid_x+IS+NOT+NULL+AND+centroid_y+IS+NOT+NULL&f=geojson',
   pointToLayer: function (feature, latlng) {
     return L.marker(latlng, {
       title: feature.properties["fqn_id"],
